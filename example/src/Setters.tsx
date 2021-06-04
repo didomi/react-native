9bcd1a380e50e508a60c181f7ad834e2527f0dd8
Juan fidel <yetelday@me.com>
2021-06-04 10:24:24 +0200

    test precommit

--- example/src/Setters.tsx
+++ example/src/Setters.tsx
@@ -11,6 +11,7 @@ export default function Getters() {
         call={async () => {
           return await Didomi.setUserConsentStatus(["analytics", "advertising_personalization"], [], [], []);
 
+
           
         }}
         test={() => {
