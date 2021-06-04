7fb9673d561604de2939b821b3fa5190edb15d18
Benjamin Horiot <horiot.benjamin@gmail.com>
2021-06-04 17:14:50 +0200

    fix: added missing files

---
+++ example/src/GetterParamsCall.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+interface GetterParamsCallProps {
+  name: string;
+  call: () => any;
+  test: (result: any) => boolean;
+}
+
+export default function GetterParams(props: GetterParamsCallProps) {
+  const [called, setCalled] = useState(false);
+  const [result, setResult] = useState(null);
+
+  return (
+    <View style={styles.container}>
+      <Button
+        onPress={async () => {
+          setResult(await props.call());
+          setCalled(true);
+        }}
+        title={props.name}
+        testID={props.name}
+      />
+      {called && (
+        <Text testID={props.name + '-result'} style={styles.result}>
+          {props.name + '-' + (props.test(result) ? 'OK' : 'KO')}
+        </Text>
+      )}
+      {result && <Text style={styles.result}>{JSON.stringify(result)}</Text>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  result: {
+    marginLeft: 10,
+  },
+});
