7e7ce475c70e6aa81479156e27e825334d6903e4
Felipe Saez <felipe.saez.victoriano@gmail.com>
2021-09-01 12:58:23 +0200

    5-getJavaScriptForWebView-extra-param-android: minor test updates

--- testApp/android/app/src/androidTest/java/com/example/reactnativedidomi/UIGettersTest.kt
+++ testApp/android/app/src/androidTest/java/com/example/reactnativedidomi/UIGettersTest.kt
@@ -21,7 +21,7 @@ class UIGettersTest: BaseUITest() {
     fun init() {
         // EVENT ON_READY NOT SENT ON SUCCESSIVE TESTS,
         // HAVE TO WAIT TO BE SURE THAT THE SDK IS READY
-        Thread.sleep(5_000L)
+        Thread.sleep(6_000L)
 
         // Make sure view is ready before starting test
         waitForDisplayed(withText("RESET"))
