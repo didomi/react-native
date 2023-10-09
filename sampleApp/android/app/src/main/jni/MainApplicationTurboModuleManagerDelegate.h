884d9d59993c9cf61dc6f98b37c4311d90021585
Nicolas CHAIX <80761828+nicolas-chaix-didomi@users.noreply.github.com>
2023-10-09 15:39:31 +0200

    Revert "CMP-4037 RN update 0.70.13 (#90)" (#91)
    This reverts commit 57e7208cbb6b985aad76985c10b802a3f6d0f687.

--- sampleApp/android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ sampleApp/android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -13,25 +13,24 @@ class MainApplicationTurboModuleManagerDelegate
           TurboModuleManagerDelegate> {
  public:
   // Adapt it to the package you used for your Java class.
-  static constexpr auto kJavaDescriptor =
-      "Lcom/rndiffapp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+  static constexpr auto kJavaDescriptor ="Lcom/example/reactnativedidomi/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
   static void registerNatives();
 
   std::shared_ptr<TurboModule> getTurboModule(
-      const std::string &name,
-      const std::shared_ptr<CallInvoker> &jsInvoker) override;
+      const std::string name,
+      const std::shared_ptr<CallInvoker> jsInvoker) override;
   std::shared_ptr<TurboModule> getTurboModule(
-      const std::string &name,
+      const std::string name,
       const JavaTurboModule::InitParams &params) override;
 
   /**
    * Test-only method. Allows user to verify whether a TurboModule can be
    * created by instances of this class.
    */
-  bool canCreateTurboModule(const std::string &name);
+  bool canCreateTurboModule(std::string name);
 };
 
 } // namespace react
