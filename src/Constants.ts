1b1a2dd0f8e64cecd19cfedcd232c018456d8a4a
Nicolas CHAIX <nicolas.chaix@didomi.io>
2023-12-12 09:54:41 +0000

    Update Native SDKs and increment minor version

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 // User agent name
 export const DIDOMI_USER_AGENT_NAME = "Didomi React-Native SDK"
 // User agent version
-export const DIDOMI_VERSION = "1.23.0"
+export const DIDOMI_VERSION = "1.24.0"
