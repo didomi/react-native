e8c6c889cb3e34f47e61b8be208243e8a51dca86
Philemon Merlet <philemon.merlet@didomi.io>
2024-01-16 12:17:44 +0100

    Deprecate old method and improve doc

--- src/Didomi.ts
+++ src/Didomi.ts
@@ -362,7 +362,7 @@ export const Didomi = {
 
   /**
    *  Determine if user status should be collected for the visitor.
-   *  Returns **true** if user status is required for the current user and one of following two conditions is met:
+   *  Returns **true** if user status is required for the current user and regulation, and one of the following two conditions is met:
    *  - User status has never been collected for this visitor yet
    *  - New user status should be collected (as new vendors have been added) AND the number of days before recollecting them has exceeded
    *  @returns: **true** if user status should be collected according to these rules, **false** otherwise.
@@ -685,6 +685,7 @@ export const Didomi = {
   /**
    *  Check if the consent should be collected depending on if we have any consents or if we have some consents but the number of days before displaying the notice again has not expired yet
    *  @returns: A boolean depending if the consent should be collected or not
+   *  @deprecated use {@link #shouldUserStatusBeCollected()} instead. The result of this method
    */
   shouldConsentBeCollected: (): Promise<boolean> =>
     RNDidomi.shouldConsentBeCollected(),
