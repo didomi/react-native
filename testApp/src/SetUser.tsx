2ea63e4ef1ab05ee38961a59d10d60d625322ac4
Philemon Merlet <philemon.merlet@didomi.io>
2024-01-17 10:31:23 +0100

    Change user id

--- testApp/src/SetUser.tsx
+++ testApp/src/SetUser.tsx
@@ -5,7 +5,7 @@ import Setter from './SetterCall';
 
 export default function SetUser() {
 
-  const userId = "d13e49f6255c8729cbb201310f49d70d65f365415a67f034b567b7eac962b944eda131376594ef5e23b025fada4e4259e953ceb45ea57a2ced7872c567e6d1fae8dcc3a9772ead783d8513032e77d3fd";
+  const userId = "hello-d13e49f6255c8729cbb201310f49d70d65f365415a67f034b567b7eac962b944eda131376594ef5e23b025fada4e4259e953ceb45ea57a2ced7872c567e6d1fae8dcc3a9772ead783d8513032e77d3fd";
   const secretId = "testsdks-PEap2wBx";
 
   return (
