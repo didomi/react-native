cac416b356f6017a84ec4fec6f8108c3f6e2d695
Felipe Saez <felipe.saez.victoriano@gmail.com>
2024-04-11 12:36:23 +0200

    Update Native SDKs and increment minor version

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 // User agent name
 export const DIDOMI_USER_AGENT_NAME = "Didomi React-Native SDK"
 // User agent version
-export const DIDOMI_VERSION = "2.1.0"
+export const DIDOMI_VERSION = "2.2.0"
