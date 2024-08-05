78673c2f1d45d35781ff22007f9b938434315a15
Nicolas CHAIX <nicolas.chaix@didomi.io>
2024-08-05 10:50:57 +0200

    cleanup code

--- sample/android/app/src/main/java/io/didomi/reactnative/sample/MainApplication.kt
+++ sample/android/app/src/main/java/io/didomi/reactnative/sample/MainApplication.kt
@@ -1,11 +1,7 @@
 package io.didomi.reactnative.sample
 
 import android.app.Application
-import com.facebook.react.PackageList
-import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
+import com.facebook.react.*
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
