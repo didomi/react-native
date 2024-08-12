02d339651745a8eee2264feab031c31bcaa6e97a
Nicolas CHAIX <nicolas.chaix@didomi.io>
2024-08-12 16:29:04 +0200

    remove old test app

--- testApp/scripts/ios/run-ui-tests-locally-ios.sh
+++
@@ -1,8 +0,0 @@
-./scripts/ios/prepare-ui-tests-locally-ios.sh || exit 1
-
-cd ios && TEST=1 && RCT_NO_LAUNCH_PACKAGER=1 xcodebuild \
--workspace DidomiExample.xcworkspace \
--scheme DidomiExample \
--sdk iphonesimulator \
--destination "platform=iOS Simulator,name=iPhone 15" \
-clean test
--- testApp/src/Types.ts
+++
@@ -1,4 +0,0 @@
-export interface TestEvent {
-  name: string;
-  data?: any;
-}
