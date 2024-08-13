4034d26877fd730bf4fb258409dcda3a0fc2dbfb
Nicolas CHAIX <nicolas.chaix@didomi.io>
2024-08-13 11:11:03 +0200

    Update iOS schemes and target

--- test/scripts/ios/run-ui-tests-locally-ios.sh
+++ test/scripts/ios/run-ui-tests-locally-ios.sh
@@ -2,7 +2,7 @@
 
 cd ios && TEST=1 && RCT_NO_LAUNCH_PACKAGER=1 xcodebuild \
 -workspace "Didomi Tests.xcworkspace" \
--scheme Didomi \
+-scheme "Didomi Tests" \
 -sdk iphonesimulator \
 -destination "platform=iOS Simulator,name=iPhone 15" \
 clean test
