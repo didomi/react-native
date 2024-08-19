b004dc52c12ed7d92ba188f28109f9154e95b3d0
Nicolas CHAIX <80761828+nicolas-chaix-didomi@users.noreply.github.com>
2024-08-19 15:20:48 +0200

    CMP-3794 - Update SDK (#128)
    * update AGP / Kotlin
    
    * update package name / clean code
    
    * update SDK dependencies (use RN 0.73.8)
    
    * update Didomi SDK listeners
    
    * use branch name as results history name for Firebase jobs
    
    * fix Didomi module
    
    * revert Didomi module `getConstants` function

--- android/src/main/java/io/didomi/reactnative/DidomiPackage.kt
+++ android/src/main/java/io/didomi/reactnative/DidomiPackage.kt
@@ -1,10 +1,9 @@
-package com.reactnativedidomi
+package io.didomi.reactnative
 
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-
 
 class DidomiPackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
