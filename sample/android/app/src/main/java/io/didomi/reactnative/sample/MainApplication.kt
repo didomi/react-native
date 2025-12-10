9ba5ad71b0990dd62f30e1eebb6627b30b19accf
Nicolas CHAIX <nicolas.chaix@didomi.io>
2025-12-10 14:19:06 +0100

    refactor(sample): - Update dependencies - Update Android gradle config and wrapper - Migrate iOS App from Obj-C to Swift

--- sample/android/app/src/main/java/io/didomi/reactnative/sample/MainApplication.kt
+++ sample/android/app/src/main/java/io/didomi/reactnative/sample/MainApplication.kt
@@ -2,10 +2,9 @@ package io.didomi.reactnative.sample
 
 import android.app.Application
 import com.facebook.react.*
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.soloader.SoLoader
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 
 class MainApplication : Application(), ReactApplication {
 
@@ -30,10 +29,6 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
-        }
+        loadReactNative(this)
     }
 }
