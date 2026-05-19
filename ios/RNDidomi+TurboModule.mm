f45247ae7650cbe79efbaeb40ceb5bd3eeb97a50
Nicolas CHAIX <nicolas.chaix@didomi.io>
2026-05-19 17:58:15 +0200

    forward-declare the Didomi interface inheriting from RCTEventEmitter (matching what Swift exposes)

--- ios/RNDidomi+TurboModule.mm
+++ ios/RNDidomi+TurboModule.mm
@@ -1,8 +1,12 @@
 #ifdef RCT_NEW_ARCH_ENABLED
 
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 #import <ReactCommon/RCTTurboModule.h>
 #import "RNDidomiSpec/RNDidomiSpec.h"
+
+@interface Didomi : RCTEventEmitter
+@end
 
 @interface Didomi (TurboModule) <NativeDidomiSpec>
 @end
